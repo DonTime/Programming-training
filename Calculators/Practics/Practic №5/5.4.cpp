447a0dc32a96d3836861a9f708fab4506a137fb3
DonTime <81639873+DonTime@users.noreply.github.com>
2022-10-28 13:38:30 +0300

    Update 5.4.cpp

---
+++ Calculators/Practics/Practic №5/5.4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+#define First float(log(pow(a,2)*x-b*x*cos(2*x)))
+#define _sin_ sin(b*x)
+#define Two float(b*(a-3.0*x)*pow(_sin_,2))
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x, a,b;
+	x = -1.5;
+	b = 2.7;
+	cout << "Введите число а";
+	cin >> a;
+	while (x<=1) {
+		if (Two == 0) {
+			cout << "Ошибка деления на 0" << endl;
+
+		}
+		else {
+			cout << "x: " << x << "  y: " << First / Two << endl;
+		}
+		x = x + 0.25;
+	}
+
+
+
+}
